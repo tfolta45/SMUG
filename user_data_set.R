523b2820fa20aff19f6f13ac41318311646ef625
Jian He <hejianpitt@gmail.com>
2018-04-18 11:05:47 -0400

    Update user_data_set.R
    Added some decoration

--- user_data_set.R
+++ user_data_set.R
@@ -3,7 +3,9 @@ library(ggplot2)
 library(dplyr)
 library(shinythemes)
 ui <- fluidPage(
-         theme = shinytheme("superhero"),
+         theme= shinytheme("superhero"),
+         sidebarLayout(
+                 
                  sidebarPanel(
                          fileInput(inputId  = "data", 
                                    label    = "Upload your data set",
@@ -16,13 +18,25 @@ ui <- fluidPage(
                                       label    = "Delimiter",
                                       choices  = c(Comma =",", Semicolon = ";", Tab = "\t"),
                                       selected = ","),
-                         radioButtons(inputId  = "disp", 
-                                      label    = "Display",
-                                      choices  = c(Head = "head", All = "all"),
-                                      selected = "head")
+                         checkboxInput(inputId  = "show_data", 
+                                       label    = "Show data table",
+                                       value    = TRUE),
+                         br(), br(),
+                         h5("Built with",
+                            img(src = "https://www.rstudio.com/wp-content/uploads/2014/04/shiny.png", height = "30px"),
+                            "by",
+                            img(src = "https://www.rstudio.com/wp-content/uploads/2014/07/RStudio-Logo-Blue-Gray.png", height = "30px"),
+                            ".")
+                         
                  ),
                  mainPanel(
-                         tableOutput(outputId = "DataFrame")
+                         tabsetPanel(id   = "tabspanel",
+                                     type = "tabs",
+                                     tabPanel(title = "Data", 
+                                              br(),
+                                              tableOutput(outputId = "DataFrame")
+                                              )
+                                     )
                          
                  )
          )
@@ -35,14 +49,17 @@ server <- function(input, output) {
                                sep    = input$dem,
                                quote  = input$quote,
                                stringsAsFactors = FALSE)
-                if(input$disp == "head") {
-                        return(head(df))
-                } 
-                else if (input$disp == "all") {
+                if(input$show_data) {
                         return(df)
-                }
+                } 
         })
         output$DataFrame<- renderTable(read_data())
+        observeEvent(input$show_data, {
+                if(input$show_data){
+                        showTab(inputId = "tabspanel", target = "Data", select = TRUE)
+                } else {
+                        hideTab(inputId = "tabspanel", target = "Data")
+                }
+        })
 }
 shinyApp(ui = ui, server = server)
- 
